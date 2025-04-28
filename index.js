26bd018bfe689284f305dc63f3f753ecc4108b57
NicoMantas <nicolasmantilla15062015@gmail.com>
2025-04-28 17:53:01 -0500

    Commit 3: Method get and post

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ app.get(
     res.send('Hello World NODEMON');
     }
 );
+
+app.post("/create-user", (req, res) => {
+    const newUser = {
+        
+    };
+});
+
+
+
+app.post("/create-user", (req, res) => {
+            //Todo lo que tengo que hacer para crear el usuario
+    //Valido JWT
+    //Verifico los datos de la req
+    //Envio los datos a la DB
+    //Envio un mensaje de exito
+    res.send('Hello World NODEMON')
+});
